b019b64f1d0f49b871accfa850435c07860768df
xxks-kkk <ferrishu3886@gmail.com>
2018-08-26 12:37:17 -0500

    Modify intellij preference & templates

---
+++ .idea/fileTemplates/internal/CMakeLists.txt.cmake
@@ -0,0 +1,10 @@
+#set( $Author = "Zeyuan Hu" )
+#set( $Email = "iamzeyuanhu@utexas.edu" )
+#
+#  > File Name:       ${FILE_NAME}
+#  > Author:          ${Author}
+#  > Mail:            ${Email}
+#  > Created Time:    ${DATE}
+#  > Description:
+#   
+#    Fill in the purpose of this source file here.
