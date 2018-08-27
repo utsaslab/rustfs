2c27dd28af719c5e03863af708e33464eea8f7d8
xxks-kkk <ferrishu3886@gmail.com>
2018-08-26 19:14:28 -0500

    Able to run hello_nvme_bdev with NVMe

--- examples/hello_nvme_bdev/hello_nvme_bdev.c
+++ examples/hello_nvme_bdev/hello_nvme_bdev.c
@@ -5,13 +5,23 @@
   > Created Time:    8/25/18
   > Description:
     
-    Similar to hello_bdev.c provided in SPDK, this program demos how to
-    create a block device and write a hello world message into it and read
-    the message from it. However, unlike hello_bdev.c, we only support NVMe
-    physical device (hello_bdev.c provides a unified interface that can support
-    various underlying device. It is done by reading in a configuration file.
-    However, doing so requires us to create block device at first through
-    a separate program. That's not what we want)
+    This program is very much the same as the hello_bdev.c provided in SPDK.
+    The program demos how to create a block device and write a hello world message into it
+    and read the message from it. hello_bdev.c provides a unified interface that can support
+    various underlying device (i.e. bdev). It is done by reading in a configuration file,
+    which contains various block device (e.g. Passthru, Malloc, NVMe)
+
+    - The explanation of configuration file:
+
+        https://github.com/spdk/spdk/blob/master/etc/spdk/nvmf.conf.in
+
+    - The explanation of various block devices:
+
+        http://www.spdk.io/doc/bdev.html#bdev_ug_introduction
+
+
+    We modify hello_bdev.c by use NVMe bdev by default and adjust the configuration file accordingly.
+    This program is also used as a testbed for the project build system.
  ************************************************************************/
 
 #include "spdk/stdinc.h"
@@ -23,18 +33,17 @@
 #include "spdk/string.h"
 #include "spdk/bdev_module.h"
 
-//int main()
-//{
-//    return 0;
-//}
-
-static char *g_bdev_name = "Malloc0";
+/*
+ * hzy: we use the NVMe bdev by default.
+ */
+static char *g_bdev_name = "Nvme0n1";
 
 /*
  * We'll use this struct to gather housekeeping hello_context to pass between
  * our events and callbacks.
  */
-struct hello_context_t {
+struct hello_context_t
+{
     struct spdk_bdev *bdev;
     struct spdk_bdev_desc *bdev_desc;
     struct spdk_io_channel *bdev_io_channel;
@@ -54,11 +63,12 @@ hello_bdev_usage(void)
 /*
  * This function is called to parse the parameters that are specific to this application
  */
-static void hello_bdev_parse_arg(int ch, char *arg)
+static void
+hello_bdev_parse_arg(int ch, char *arg)
 {
-    switch (ch) {
-        case 'b':
-            g_bdev_name = arg;
+    switch (ch)
+    {
+        case 'b':g_bdev_name = arg;
             break;
     }
 }
@@ -71,9 +81,12 @@ read_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
 {
     struct hello_context_t *hello_context = cb_arg;
 
-    if (success) {
+    if (success)
+    {
         SPDK_NOTICELOG("Read string from bdev : %s\n", hello_context->buff);
-    } else {
+    }
+    else
+    {
         SPDK_ERRLOG("bdev io read error\n");
     }
 
@@ -98,9 +111,12 @@ write_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
     /* Complete the I/O */
     spdk_bdev_free_io(bdev_io);
 
-    if (success) {
+    if (success)
+    {
         SPDK_NOTICELOG("bdev io write completed successfully\n");
-    } else {
+    }
+    else
+    {
         SPDK_ERRLOG("bdev io write error: %d\n", EIO);
         spdk_put_io_channel(hello_context->bdev_io_channel);
         spdk_bdev_close(hello_context->bdev_desc);
@@ -116,7 +132,8 @@ write_complete(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
     rc = spdk_bdev_read(hello_context->bdev_desc, hello_context->bdev_io_channel,
                         hello_context->buff, 0, blk_size, read_complete, hello_context);
 
-    if (rc) {
+    if (rc)
+    {
         SPDK_ERRLOG("%s error while reading from bdev: %d\n", spdk_strerror(-rc), rc);
         spdk_put_io_channel(hello_context->bdev_io_channel);
         spdk_bdev_close(hello_context->bdev_desc);
@@ -140,12 +157,23 @@ hello_start(void *arg1, void *arg2)
     SPDK_NOTICELOG("Successfully started the application\n");
 
     /*
+     * hzy: here, we list all possible bdev that we can use.
+     */
+    for (struct spdk_bdev *first = spdk_bdev_first(); first != NULL; first = spdk_bdev_next(first))
+    {
+        SPDK_NOTICELOG("bdev name: %s\n", first->name);
+        SPDK_NOTICELOG("bdev product_name: %s\n", first->product_name);
+        SPDK_NOTICELOG("bdev module name: %s\n", first->module->name);
+    }
+
+    /*
      * Get the bdev. There can be many bdevs configured in
      * in the configuration file but this application will only
      * use the one input by the user at runtime so we get it via its name.
      */
     hello_context->bdev = spdk_bdev_get_by_name(hello_context->bdev_name);
-    if (hello_context->bdev == NULL) {
+    if (hello_context->bdev == NULL)
+    {
         SPDK_ERRLOG("Could not find the bdev: %s\n", hello_context->bdev_name);
         spdk_app_stop(-1);
         return;
@@ -157,7 +185,8 @@ hello_start(void *arg1, void *arg2)
      */
     SPDK_NOTICELOG("Opening the bdev %s\n", hello_context->bdev_name);
     rc = spdk_bdev_open(hello_context->bdev, true, NULL, NULL, &hello_context->bdev_desc);
-    if (rc) {
+    if (rc)
+    {
         SPDK_ERRLOG("Could not open bdev: %s\n", hello_context->bdev_name);
         spdk_app_stop(-1);
         return;
@@ -166,7 +195,8 @@ hello_start(void *arg1, void *arg2)
     SPDK_NOTICELOG("Opening io channel\n");
     /* Open I/O channel */
     hello_context->bdev_io_channel = spdk_bdev_get_io_channel(hello_context->bdev_desc);
-    if (hello_context->bdev_io_channel == NULL) {
+    if (hello_context->bdev_io_channel == NULL)
+    {
         SPDK_ERRLOG("Could not create bdev I/O channel!!\n");
         spdk_bdev_close(hello_context->bdev_desc);
         spdk_app_stop(-1);
@@ -179,7 +209,8 @@ hello_start(void *arg1, void *arg2)
     blk_size = spdk_bdev_get_block_size(hello_context->bdev);
     buf_align = spdk_bdev_get_buf_align(hello_context->bdev);
     hello_context->buff = spdk_dma_zmalloc(blk_size, buf_align, NULL);
-    if (!hello_context->buff) {
+    if (!hello_context->buff)
+    {
         SPDK_ERRLOG("Failed to allocate buffer\n");
         spdk_put_io_channel(hello_context->bdev_io_channel);
         spdk_bdev_close(hello_context->bdev_desc);
@@ -191,7 +222,8 @@ hello_start(void *arg1, void *arg2)
     SPDK_NOTICELOG("Writing to the bdev\n");
     rc = spdk_bdev_write(hello_context->bdev_desc, hello_context->bdev_io_channel,
                          hello_context->buff, 0, blk_size, write_complete, hello_context);
-    if (rc) {
+    if (rc)
+    {
         SPDK_ERRLOG("%s error while writing to bdev: %d\n", spdk_strerror(-rc), rc);
         spdk_bdev_close(hello_context->bdev_desc);
         spdk_put_io_channel(hello_context->bdev_io_channel);
@@ -210,7 +242,7 @@ main(int argc, char **argv)
     /* Set default values in opts structure. */
     spdk_app_opts_init(&opts);
     opts.name = "hello_bdev";
-    opts.config_file = "/home/zeyuanhu/rustfs/examples/hello_nvme_bdev/bdev.conf";
+    opts.config_file = "bdev.conf";
 
     /*
      * The user can provide the config file and bdev name at run time.
@@ -218,11 +250,12 @@ main(int argc, char **argv)
      * ./hello_bdev -c bdev.conf -b Malloc0
      * To use passthru bdev PT0 run with params
      * ./hello_bdev -c bdev.conf -b PT0
-     * If none of the parameters are provide the application will use the
-     * default parameters(-c bdev.conf -b Malloc0).
+     * hzy: If none of the parameters are provide the application will use the
+     * default parameters(-c bdev.conf -b Nvme0n1).
      */
     if ((rc = spdk_app_parse_args(argc, argv, &opts, "b:", NULL, hello_bdev_parse_arg,
-                                  hello_bdev_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
+                                  hello_bdev_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS)
+    {
         exit(rc);
     }
     hello_context.bdev_name = g_bdev_name;
@@ -234,7 +267,8 @@ main(int argc, char **argv)
      * spdk_app_start() before hello_start() runs.
      */
     rc = spdk_app_start(&opts, hello_start, &hello_context, NULL);
-    if (rc) {
+    if (rc)
+    {
         SPDK_ERRLOG("ERROR starting application\n");
     }
 
