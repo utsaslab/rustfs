b056796134a69ec4650d93ee73183051b43da9f7
xxks-kkk <ferrishu3886@gmail.com>
2018-08-29 16:02:14 -0500

    remove hardcoded include path; now depends on SPDK_DIR

--- rust-spdk/build.rs
+++ rust-spdk/build.rs
@@ -4,17 +4,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let spdk_dir = match env::var_os("SPDK_DIR") {
+        Some(val) => val.into_string().unwrap(),
+        None => panic!("SPDK_DIR is not defined in the environment")
+    };
+    let include_path = format!("-I{}/include", spdk_dir);
+
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        .clang_arg("-I/home/zeyuanhu/spdk_install/include")
+        .clang_arg(include_path)
         // The input header we would like to generate
         // bindings for.
         .header("src/wrapper.h")
         .blacklist_type("IPPORT_.*")   // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
         .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
-        .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687#issuecomment-416870573
+        .opaque_type("spdk_nvme_feat_async_event_configuration") // https://github.com/rust-lang-nursery/rust-bindgen/issues/687
         .opaque_type("spdk_nvme_feat_async_event_configuration__bindgen_ty_1")
         // Finish the builder and generate the bindings.
         .generate()
