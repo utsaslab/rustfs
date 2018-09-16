ab12c7c537b49e35b9db3b3edcc025bb67074b92
xxks-kkk <ferrishu3886@gmail.com>
2018-09-16 12:49:23 -0500

    get the basic FFI-related repo structure setup; mainly reference git2-rs repo

---
+++ spdk-rs/src/bdev.rs
@@ -0,0 +1,10 @@
+/*************************************************************************
+  > File Name:       bdev.rs
+  > Author:          Zeyuan Hu
+  > Mail:            iamzeyuanhu@utexas.edu
+  > Created Time:    9/16/18
+  > Description:
+    
+    FFI for "bdev.h"
+ ************************************************************************/
+
