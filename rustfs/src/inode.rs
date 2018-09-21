37221668a56e8e1c68accb06aee98f4674e6a9d2
xxks-kkk <ferrishu3886@gmail.com>
2018-09-21 14:27:40 -0500

    port the source of SergioBenitez/RustFS to the repo

--- rustfs/src/inode.rs
+++ rustfs/src/inode.rs
@@ -7,3 +7,236 @@
     
     Fill in the purpose of this source file here.
  ************************************************************************/
+
+use time;
+use time::Timespec;
+use std::mem;
+use std::ptr;
+use std::ptr::copy_nonoverlapping;
+
+const PAGE_SIZE: usize = 4096;
+const LIST_SIZE: usize = 256;
+
+type Page = Box<([u8; PAGE_SIZE])>;
+type Entry = Page;
+type EntryList = TList<Entry>; // TODO: Option<TList> for lazy loading
+type DoubleEntryList = TList<EntryList>;
+pub type TList<T> = Box<([Option<T>; LIST_SIZE])>;
+
+#[inline(always)]
+fn ceil_div(x: usize, y: usize) -> usize {
+    return (x + y - 1) / y;
+}
+
+#[inline(always)]
+pub fn create_tlist<T>() -> TList<T> {
+    let mut list: TList<T> = Box::new(unsafe { mem::uninitialized() });
+    for x in list.iter_mut() { unsafe { ptr::write(x, None); } };
+    list
+}
+
+pub struct Inode {
+    single: EntryList, // Box<([Option<Page>, ..256])>
+    double: DoubleEntryList, // Box<[Option<Box<([Option<Page>>, ..256])>, ..256]
+    size: usize,
+
+    mod_time: Timespec,
+    access_time: Timespec,
+    create_time: Timespec,
+}
+
+impl Inode {
+    pub fn new() -> Inode {
+        let time_now = time::get_time();
+
+        Inode {
+            single: create_tlist(),
+            double: create_tlist(),
+            size: 0,
+
+            mod_time: time_now,
+            access_time: time_now,
+            create_time: time_now
+        }
+    }
+
+    fn get_or_alloc_page<'a>(&'a mut self, num: usize) -> &'a mut Page {
+        if num >= LIST_SIZE + LIST_SIZE * LIST_SIZE {
+            panic!("Maximum file size exceeded!")
+        };
+
+        // Getting a pointer to the page
+        let page = if num < LIST_SIZE {
+            // if the page num is in the singly-indirect list
+            &mut self.single[num]
+        } else {
+            // if the page num is in the doubly-indirect list. We allocate a new
+            // entry list where necessary (*entry_list = ...)
+            let double_entry = num - LIST_SIZE;
+            let slot = double_entry / LIST_SIZE;
+            let entry_list = &mut self.double[slot];
+
+            match *entry_list {
+                None => *entry_list = Some(create_tlist()),
+                _ => { /* Do nothing */ }
+            }
+
+            let entry_offset = double_entry % LIST_SIZE;
+            &mut entry_list.as_mut().unwrap()[entry_offset]
+        };
+
+        match *page {
+            None => *page = Some(Box::new([0u8; 4096])),
+            _ => { /* Do Nothing */ }
+        }
+
+        page.as_mut().unwrap()
+    }
+
+    fn get_page<'a>(&'a self, num: usize) -> &'a Option<Page> {
+        if num >= LIST_SIZE + LIST_SIZE * LIST_SIZE {
+            panic!("Page does not exist.")
+        };
+
+        if num < LIST_SIZE {
+            &self.single[num]
+        } else {
+            let double_entry = num - LIST_SIZE;
+            let slot = double_entry / LIST_SIZE;
+            let entry_offset = double_entry % LIST_SIZE;
+            let entry_list = &self.double[slot];
+
+            match *entry_list {
+                None => panic!("Page does not exist."),
+                _ => &entry_list.as_ref().unwrap()[entry_offset]
+            }
+        }
+    }
+
+    pub fn write(&mut self, offset: usize, data: &[u8]) -> usize {
+        let mut written = 0;
+        let mut block_offset = offset % PAGE_SIZE; // offset from first block
+
+        let start = offset / PAGE_SIZE; // first block to act on
+        let blocks_to_act_on = ceil_div(block_offset + data.len(), PAGE_SIZE);
+
+        for i in 0..blocks_to_act_on {
+            // Resetting the block offset after first pass since we want to read from
+            // the beginning of the block after the first time.
+            if block_offset != 0 && i > 0 { block_offset = 0 };
+
+            // Need to account for offsets from first and last blocks
+            let num_bytes = if i == blocks_to_act_on - 1 {
+                data.len() - written
+            } else {
+                PAGE_SIZE - block_offset
+            };
+
+            // Finding our block, writing to it
+            let mut page = self.get_or_alloc_page(start + i);
+            let slice = &mut page[block_offset..(block_offset + num_bytes)];
+            // written += slice.copy_from(data.slice(written, written + num_bytes));
+            unsafe {
+                // TODO: This may be extremely slow! Use copy_nonoverlapping, perhaps.
+                let src = data[written..(written + num_bytes)].as_ptr();
+                copy_nonoverlapping(src, slice.as_mut_ptr(), num_bytes);
+            }
+
+            written += num_bytes;
+        }
+
+        let last_byte = offset + written;
+        if self.size < last_byte { self.size = last_byte; }
+
+        let time_now = time::get_time();
+        self.mod_time = time_now;
+        self.access_time = time_now;
+
+        written
+    }
+
+    pub fn read(&self, offset: usize, data: &mut [u8]) -> usize {
+        let mut read = 0;
+        let mut block_offset = offset % PAGE_SIZE; // offset from first block
+        let start = offset / PAGE_SIZE; // first block to act on
+        let blocks_to_act_on = ceil_div(block_offset + data.len(), PAGE_SIZE);
+
+        for i in 0..blocks_to_act_on {
+            // Resetting the block offset after first pass since we want to read from
+            // the beginning of the block after the first time.
+            if block_offset != 0 && i > 0 { block_offset = 0 };
+
+            // Need to account for offsets from first and last blocks
+            let num_bytes = if i == blocks_to_act_on - 1 {
+                data.len() - read
+            } else {
+                PAGE_SIZE - block_offset
+            };
+
+            // Finding our block, reading from it
+            let page = match self.get_page(start + i) {
+                &None => panic!("Empty data."),
+                &Some(ref pg) => pg
+            };
+
+            let slice = &mut data[read..(read + num_bytes)];
+            // read += slice.copy_from(page.slice(block_offset,
+            // block_offset + num_bytes));
+            unsafe {
+                // copy_from is extremely slow! use copy_memory instead
+                let src = page[block_offset..(block_offset + num_bytes)].as_ptr();
+                copy_nonoverlapping(src, slice.as_mut_ptr(), num_bytes);
+            }
+
+            read += num_bytes;
+        }
+
+        read
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn stat(&self) -> (Timespec, Timespec, Timespec) {
+        (self.create_time, self.access_time, self.mod_time)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+
+    use super::{Inode};
+    use self::rand::random;
+    use time;
+
+    fn rand_array(size: usize) -> Vec<u8> {
+        (0..size).map(|_| random::<u8>()).collect()
+    }
+
+    #[test]
+    fn test_simple_write() {
+        const SIZE: usize = 4096 * 8 + 3434;
+
+        let original_data = rand_array(SIZE);
+        let time_now = time::get_time();
+        let mut inode = Inode::new();
+        let mut buf = [0u8; SIZE];
+
+        // Write the random data, read it back into buffer
+        inode.write(0, original_data.as_slice());
+        inode.read(0, &mut buf);
+
+        // Make sure inode is right size
+        assert_eq!(SIZE, inode.size());
+
+        // Make sure contents are correct
+        for i in 0..SIZE {
+            assert_eq!(buf[i], original_data[i]);
+        }
+
+        let (create, _, _) = inode.stat();
+        assert_eq!(create.sec, time_now.sec);
+    }
+}
--- rustfs/src/lib.rs
+++ rustfs/src/lib.rs
@@ -8,10 +8,295 @@
     Fill in the purpose of this source file here.
  ************************************************************************/
 
+extern crate time;
+
+mod directory;
+mod file;
+mod inode;
+
+use file::{File, FileHandle};
+use file::File::{EmptyFile, DataFile, Directory};
+use std::rc::Rc;
+use std::cell::{RefCell};
+use std::collections::HashMap;
+use directory::DirectoryHandle;
+pub use file::Whence;
+pub use inode::Inode;
+
+pub type FileDescriptor = isize;
+
+pub const O_RDONLY: u32 =   (1 << 0);
+pub const O_WRONLY: u32 =   (1 << 1);
+pub const O_RDWR: u32 =     (1 << 2);
+pub const O_NONBLOCK: u32 = (1 << 3);
+pub const O_APPEND: u32 =   (1 << 4);
+pub const O_CREAT: u32 =    (1 << 5);
+
+pub struct Proc<'r> {
+    cwd: File<'r>,
+    fd_table: HashMap<FileDescriptor, FileHandle<'r>>,
+    fds: Vec<FileDescriptor>
+}
+
+impl<'r> Proc<'r> {
+    pub fn new() -> Proc<'r> {
+        Proc {
+            cwd: File::new_dir(None),
+            fd_table: HashMap::new(),
+            fds: (0..(256 - 2)).map(|i| 256 - i).collect(),
+        }
+    }
+
+    #[inline(always)]
+    fn extract_fd(fd_opt: &Option<FileDescriptor>) -> FileDescriptor {
+        match fd_opt {
+            &Some(fd) => fd,
+            &None => panic!("Error in FD allocation.")
+        }
+    }
+
+    pub fn open(&mut self, path: &'r str, flags: u32) -> FileDescriptor {
+        let lookup = self.cwd.get(path);
+        let file = match lookup {
+            Some(f) => f,
+            None => {
+                if (flags & O_CREAT) != 0 {
+                    // FIXME: Fetch from allocator
+                    let rcinode = Rc::new(RefCell::new(Box::new(Inode::new())));
+                    let file = File::new_data_file(rcinode);
+                    self.cwd.insert(path, file.clone());
+                    file
+                } else {
+                    EmptyFile
+                }
+            }
+        };
+
+        match file {
+            DataFile(_) => {
+                let fd = Proc::extract_fd(&self.fds.pop());
+                let handle = FileHandle::new(file);
+                self.fd_table.insert(fd, handle);
+                fd
+            }
+            Directory(_) => -1,
+            EmptyFile => -2,
+        }
+    }
+
+    pub fn read(&self, fd: FileDescriptor, dst: &mut [u8]) -> usize {
+        let handle = self.fd_table.get(&fd).expect("fd does not exist");
+        handle.read(dst)
+    }
+
+    pub fn write(&mut self, fd: FileDescriptor, src: &[u8]) -> usize {
+        let handle = self.fd_table.get_mut(&fd).expect("fd does not exist");
+        handle.write(src)
+    }
+
+    pub fn seek(&mut self, fd: FileDescriptor, o: isize, whence: Whence) -> usize {
+        let handle = self.fd_table.get_mut(&fd).expect("fd does not exist");
+        handle.seek(o, whence)
+    }
+
+    pub fn close(&mut self, fd: FileDescriptor) {
+        self.fd_table.remove(&fd);
+        self.fds.push(fd);
+    }
+
+    pub fn unlink(&mut self, path: &'r str) {
+        self.cwd.remove(path);
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod proc_tests {
+    // extern crate test;
+    extern crate rand;
+
+    use super::{Proc, O_RDWR, O_CREAT};
+    use file::Whence::SeekSet;
+    use inode::Inode;
+    use self::rand::random;
+
+    static mut test_inode_drop: bool = false;
+
+    impl Drop for Inode {
+        fn drop(&mut self) {
+            unsafe {
+                if test_inode_drop {
+                    test_inode_drop = false;
+                    panic!("Dropping.");
+                } else {
+                    println!("Dropping, but no flag.");
+                }
+            }
+        }
+    }
+
+    fn rand_array(size: usize) -> Vec<u8> {
+        (0..size).map(|_| random::<u8>()).collect()
+    }
+
+    fn assert_eq_buf(first: &[u8], second: &[u8]) {
+        assert_eq!(first.len(), second.len());
+
+        for i in 0..first.len() {
+            assert_eq!(first[i], second[i]);
+        }
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn simple_test() {
+        const SIZE: usize = 4096 * 8 + 3434;
+        let mut p = Proc::new();
+        let data = rand_array(SIZE);
+        let mut buf = [0u8; SIZE];
+        let filename = "first_file";
+
+        let fd = p.open(filename, O_RDWR | O_CREAT);
+        p.write(fd, &data);
+        p.seek(fd, 0, SeekSet);
+        p.read(fd, &mut buf);
+
+        assert_eq_buf(&data, &buf);
+
+        let fd2 = p.open(filename, O_RDWR);
+        let mut buf2 = [0u8; SIZE];
+        p.read(fd2, &mut buf2);
+
+        assert_eq_buf(&data, &buf2);
+
+        p.close(fd);
+        p.close(fd2);
+
+        let fd3 = p.open(filename, O_RDWR);
+        let mut buf3 = [0u8; SIZE];
+        p.read(fd3, &mut buf3);
+
+        assert_eq_buf(&data, &buf3);
+        p.close(fd3);
+
+        p.unlink(filename);
+
+        let fd4 = p.open(filename, O_RDWR);
+        assert_eq!(fd4, -2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_proc_drop_inode_dealloc() {
+        // Variable is used to make sure that the Drop implemented is only valid for
+        // tests that set that test_inode_drop global variable to true.
+        unsafe { test_inode_drop = true; }
+
+        const SIZE: usize = 4096 * 3 + 3498;
+        let mut p = Proc::new();
+        let mut data = rand_array(SIZE);
+
+        let fd = p.open("file", O_RDWR | O_CREAT);
+        p.write(fd, &mut data);
+    }
+
+    /**
+     * This function makes sure that on unlink, the inode's data structure is
+     * indeed dropped. This means that a few things have gone right:
+     *
+     * 1) The FileHandle was dropped. If it wasn't, it would hold a reference to
+     *    the file and so the file wouldn't be dropped. This should happen on
+     *    close.
+     * 2) The File, containing the Inode, was dropped. This should happen on
+     *    unlink.
+     */
+    #[test]
+    #[should_panic]
+    fn test_inode_dealloc() {
+        // Make sure flag is set to detect drop.
+        unsafe { test_inode_drop = true; }
+
+        const SIZE: usize = 4096 * 3 + 3498;
+        let mut p = Proc::new();
+        let mut data = rand_array(SIZE);
+        let mut buf = [0u8; SIZE];
+        let filename = "first_file";
+
+        let fd = p.open(filename, O_RDWR | O_CREAT);
+        p.write(fd, &mut data);
+        p.seek(fd, 0, SeekSet);
+        p.read(fd, &mut buf);
+
+        assert_eq_buf(&data, &buf);
+
+        // close + unlink should remove both references to inode, dropping it,
+        // causing a failure
+        p.close(fd);
+        p.unlink(filename);
+
+        // If inode is not being dropped properly, ie, on the unlink call this will
+        // cause a double failure: once for panic! call, and once when then the Inode
+        // is dropped since the Proc structure will be dropped.
+        //
+        // To test that RC is working properly, make sure that a double failure
+        // occurs when either the close or unlink calls above are commented out.
+        panic!("Inode not dropped!");
+    }
+
+    #[test]
+    fn test_max_singly_file_size() {
+        const SIZE: usize = 4096 * 256;
+        let mut p = Proc::new();
+        let mut data = rand_array(SIZE);
+        let mut buf = [0u8; SIZE];
+        let filename = "first_file";
+
+        let fd = p.open(filename, O_RDWR | O_CREAT);
+        p.write(fd, &mut data);
+        p.seek(fd, 0, SeekSet);
+        p.read(fd, &mut buf);
+
+        assert_eq_buf(&data, &buf);
+
+        p.close(fd);
+        p.unlink(filename);
+
+        let fd4 = p.open(filename, O_RDWR);
+        assert_eq!(fd4, -2);
+    }
+
+    #[test]
+    fn test_max_file_size() {
+        const SIZE: usize = 2 * 4096 * 256;
+        let mut p = Proc::new();
+        let mut data1 = rand_array(SIZE);
+        let mut data2 = rand_array(SIZE);
+        let mut buf = vec![0; SIZE];
+        let filename = "first_file";
+
+        let fd = p.open(filename, O_RDWR | O_CREAT);
+        p.write(fd, &mut data1);
+        p.seek(fd, 4096 * 257 * 256 - SIZE as isize, SeekSet);
+        p.write(fd, &mut data2);
+
+        p.seek(fd, 0, SeekSet);
+        p.read(fd, &mut buf);
+        assert_eq_buf(&data1, &buf);
+
+        p.seek(fd, 4096 * 257 * 256 - SIZE as isize, SeekSet);
+        p.read(fd, &mut buf);
+        assert_eq_buf(&data2, &buf);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_morethan_max_file_size() {
+        const SIZE: usize = 2 * 4096 * 256;
+        let mut p = Proc::new();
+        let mut data = rand_array(SIZE);
+        let filename = "first_file";
+
+        let fd = p.open(filename, O_RDWR | O_CREAT);
+        p.write(fd, &mut data);
+        p.seek(fd, 4096 * 257 * 256 + 1 - SIZE as isize, SeekSet);
+        p.write(fd, &mut data);
     }
 }
