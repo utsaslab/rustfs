c5b92c8a9cd8796bf61af16a24d61b7e2fc7b1be
xxks-kkk <ferrishu3886@gmail.com>
2018-09-21 14:14:54 -0500

    Start to port SergioBenitez/RustFS

---
+++ rustfs/src/directory.rs
@@ -0,0 +1,9 @@
+/*************************************************************************
+  > File Name:       directory.rs
+  > Author:          Zeyuan Hu
+  > Mail:            iamzeyuanhu@utexas.edu
+  > Created Time:    9/21/18
+  > Description:
+    
+    Fill in the purpose of this source file here.
+ ************************************************************************/
---
+++ rustfs/src/lib.rs
@@ -0,0 +1,17 @@
+/*************************************************************************
+  > File Name:       lib.rs
+  > Author:          Zeyuan Hu
+  > Mail:            iamzeyuanhu@utexas.edu
+  > Created Time:    9/21/18
+  > Description:
+    
+    Fill in the purpose of this source file here.
+ ************************************************************************/
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
