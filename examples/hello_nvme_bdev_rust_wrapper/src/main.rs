4893b3f0ef3fe54a16846b544cb5831b1b534aa7
xxks-kkk <ferrishu3886@gmail.com>
2018-10-08 12:03:00 -0500

    Remove AppContextExt because we still need to pass the context into the hello_start function (ie. we cannot reference the context object directly)

--- examples/hello_nvme_bdev_rust_wrapper/src/main.rs
+++ examples/hello_nvme_bdev_rust_wrapper/src/main.rs
@@ -14,33 +14,26 @@ extern crate spdk_rs;
 
 use spdk_rs::{AppOpts, AppContext, app_stop, Bdev};
 
-// https://stackoverflow.com/questions/33376486/is-there-a-way-other-than-traits-to-add-methods-to-a-type-i-dont-own
-trait AppContextExt {
-    fn hello_start(context : &AppContext);
-}
-
-impl AppContextExt for AppContext {
-    fn hello_start(context : &AppContext) {
-        println!("Successfully started the application");
-        let mut first_bdev = Bdev::spdk_bdev_first();
-        while  !first_bdev.is_none() {
-            let mut bdev = first_bdev.unwrap();
-            println!("bdev name: {}", bdev.name());
-            first_bdev = Bdev::spdk_bdev_next(&bdev);
-        }
-        let bdev_name = context.get_bdev_name();
-        let bdev = Bdev::spdk_bdev_get_by_name(bdev_name);
-        match bdev {
-            Err(E) => {
-                let s = E.to_owned();
-                let s_slice = &s[..];
-                println!("{}", E);
-                app_stop(false);
-            }
-            Ok(T) => {}
-        }
-        app_stop(true);
+fn hello_start(context: &AppContext) {
+    println!("Successfully started the application");
+    let mut first_bdev = Bdev::spdk_bdev_first();
+    while !first_bdev.is_none() {
+        let mut bdev = first_bdev.unwrap();
+        println!("bdev name: {}", bdev.name());
+        first_bdev = Bdev::spdk_bdev_next(&bdev);
     }
+    let bdev_name = context.get_bdev_name();
+    let bdev = Bdev::spdk_bdev_get_by_name(bdev_name);
+    match bdev {
+        Err(E) => {
+            let s = E.to_owned();
+            let s_slice = &s[..];
+            println!("{}", E);
+            app_stop(false);
+        }
+        Ok(T) => {}
+    }
+    app_stop(true);
 }
 
 fn main()
@@ -54,6 +47,6 @@ fn main()
     context.bdev_name("Nvme0n1");
 
     let ret = opts.start(|| {
-        spdk_rs::AppContext::hello_start(&context);
+        hello_start(&context);
     }, &context);
 }
