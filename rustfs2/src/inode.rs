5eee23c97ae1f40283cddbc53777b216bd41d703
YuhZheng <yzheng@cs.utexas.edu>
2019-05-10 00:54:39 -0500

    fix BLOCK_SIZE

--- rustfs2/src/inode.rs
+++ rustfs2/src/inode.rs
@@ -51,11 +51,12 @@ impl Inode {
     async fn read_inode(&self) {
         let fs = fs_internal.unwrap();
         let offset = fs.inode_base + self.inum * INODE_SIZE;
-        let blk = offset / BLOCK_SIZE;
-        let blk_offset = offset % BLOCK_SIZE;
-        let mut read_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
-        await!(fs.device.read(&mut read_buf, blk, BLOCK_SIZE));
-        let mut buf = read_buf.read_bytes(BLOCK_SIZE);
+        let blk_size = fs.device.blk_size();
+        let blk = offset / blk_size;
+        let blk_offset = offset % blk_size;
+        let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
+        await!(fs.device.read(&mut read_buf, blk, blk_size));
+        let mut buf = read_buf.read_bytes(blk_size);
         let mut content = &buf[blk_offset..blk_offset + INODE_SIZE];
         unsafe {
             self.dirtype = mem::transmute::<[u8; 8], usize>(*array_ref![content, 0, 8]);
@@ -69,11 +70,10 @@ impl Inode {
         }
     }
 
-    pub fn read_file_from_inum(inum: usize) -> File {
-        let device = dev.unwrap();
-        let inode_base = fs_internal.unwrap().inode_base;
-        let blk_size = device.blk_size();
-        let offset = inode_base + inum * INODE_SIZE;
+    async pub fn read_file_from_inum(inum: usize) -> File {
+        let fs = fs_internal.unwrap();
+        let blk_size = fs.device.blk_size();
+        let offset = fs.inode_base + inum * INODE_SIZE;
         let blk = offset / blk_size;
         let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, device.blk_align());
         await!(device.read(&read_buf, blk, blk_size))?;
@@ -86,6 +86,7 @@ impl Inode {
             let single = mem::transmute::<[u8; 8], usize>(*array_ref![content, 16, 8]);
             let double = mem::transmute::<[u8; 8], usize>(*array_ref![content, 24, 8]);
             inode = Inode {
+                inum: inum,
                 dirtype: dirtype,
                 size: size,
                 single: Some(single),
@@ -118,12 +119,13 @@ impl Inode {
     async fn write_inode(&self) {
         // TODO: add unit test
         let fs = fs_internal.unwrap();
+        let blk_size = fs.device.blk_size();
         let offset = fs.inode_base + self.inum * INODE_SIZE;
-        let blk = offset / BLOCK_SIZE;
-        let blk_offset = offset % BLOCK_SIZE;
-        let mut read_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
-        await!(fs.device.read(&mut read_buf, blk, BLOCK_SIZE));
-        let mut buf = read_buf.read_bytes(BLOCK_SIZE);
+        let blk = offset / blk_size;
+        let blk_offset = offset % blk_size;
+        let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
+        await!(fs.device.read(&mut read_buf, blk, blk_size));
+        let mut buf = read_buf.read_bytes(blk_size);
         let mut content = &buf[blk_offset..blk_offset + INODE_SIZE];
         unsafe {
             let tmp = mem::transmute::<usize, [u8; 8]>(self.dirtype);
@@ -137,12 +139,13 @@ impl Inode {
         }
         let mut write_buf = read_buf;
         write_buf.fill_bytes(buf);
-        await!(fs.device.write(&write_buf, blk, BLOCK_SIZE));
+        await!(fs.device.write(&write_buf, blk, blk_size));
     }
 
     /// read inode metadata and return block number
     async fn get_or_alloc_page(&mut self, num: usize) -> usize {
         let fs = fs_internal.unwrap();
+        let blk_size = fs.device.blk_size();
         if num >= LIST_SIZE + 1 {
             panic!("Maximum file size exceeded!")
         };
@@ -166,16 +169,16 @@ impl Inode {
             // entry list where necessary (*entry_list = ...)
             let index = num - 1;
             if self.double.is_none() {
-                //                if self.size <= BLOCK_SIZE {
+                //                if self.size <= blk_size {
                 self.double = Some(await!(FsInternal::alloc_block()));
                 need_update = true;
                 //                }else{
                 //                }
             }
-            let mut read_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
-            let offset = fs.data_base + self.double.unwrap() * BLOCK_SIZE;
-            await!(fs.device.read(&mut read_buf, offset, BLOCK_SIZE));
-            let entry = Inode::parse_entry(read_buf.read_bytes(BLOCK_SIZE), index);
+            let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
+            let offset = fs.data_base + self.double.unwrap() * blk_size;
+            await!(fs.device.read(&mut read_buf, offset, blk_size));
+            let entry = Inode::parse_entry(read_buf.read_bytes(blk_size), index);
             entry
         };
 
@@ -187,7 +190,8 @@ impl Inode {
 
     async fn get_page(&self, num: usize) -> usize {
         let fs = fs_internal.unwrap();
-        if num * BLOCK_SIZE >= self.size {
+        let blk_size = fs.device.blk_size();
+        if num * blk_size >= self.size {
             panic!("Page does not exist.")
         };
         await!(self.read_inode());
@@ -195,10 +199,10 @@ impl Inode {
             0
         } else {
             let index = num - 1;
-            let mut read_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
-            let offset = fs.data_base + self.double.unwrap() * BLOCK_SIZE;
-            await!(fs.device.read(&mut read_buf, offset, BLOCK_SIZE));
-            let entry = Inode::parse_entry(read_buf.read_bytes(BLOCK_SIZE), index);
+            let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
+            let offset = fs.data_base + self.double.unwrap() * blk_size;
+            await!(fs.device.read(&mut read_buf, offset, blk_size));
+            let entry = Inode::parse_entry(read_buf.read_bytes(blk_size), index);
             entry
             // TODO: read the indirect block
         }
@@ -206,11 +210,12 @@ impl Inode {
 
     async fn write<'a>(&'a mut self, offset: usize, data: &'a [u8]) -> usize {
         let fs = fs_internal.unwrap();
+        let blk_size = fs.device.blk_size();
         let mut written: usize = 0;
-        let mut block_offset = offset % BLOCK_SIZE; // offset from first block
+        let mut block_offset = offset % blk_size; // offset from first block
 
-        let start = offset / BLOCK_SIZE; // first block to act on
-        let blocks_to_act_on = ceil_div(block_offset + data.len(), BLOCK_SIZE);
+        let start = offset / blk_size; // first block to act on
+        let blocks_to_act_on = ceil_div(block_offset + data.len(), blk_size);
 
         for i in 0..blocks_to_act_on {
             // Resetting the block offset after first pass since we want to read from
@@ -223,17 +228,17 @@ impl Inode {
             let num_bytes = if i == blocks_to_act_on - 1 {
                 data.len() - written
             } else {
-                BLOCK_SIZE - block_offset
+                blk_size - block_offset
             };
 
             // Finding our block, writing to it
             let page = await!(self.get_or_alloc_page(start + i));
 
             // TODO: check this!
-            let pg_offset = fs.data_base + page * BLOCK_SIZE;
-            let mut read_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
-            await!(fs.device.read(&mut read_buf, pg_offset, BLOCK_SIZE));
-            let disk_page = read_buf.read_bytes(BLOCK_SIZE);
+            let pg_offset = fs.data_base + page * blk_size;
+            let mut read_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
+            await!(fs.device.read(&mut read_buf, pg_offset, blk_size));
+            let disk_page = read_buf.read_bytes(blk_size);
             // let slice = array_mut_ref![disk_page, block_offset, num_bytes];
             let mut slice = &mut disk_page[block_offset..(block_offset + num_bytes)];
             // written += slice.copy_from(data.slice(written, written + num_bytes));
@@ -242,9 +247,9 @@ impl Inode {
                 let src = data[written..(written + num_bytes)].as_ptr();
                 copy_nonoverlapping(src, slice.as_mut_ptr(), num_bytes);
             }
-            let mut write_buf = spdk_rs::env::dma_zmalloc(BLOCK_SIZE, 0);
+            let mut write_buf = spdk_rs::env::dma_zmalloc(blk_size, 0);
             write_buf.fill_bytes(disk_page);
-            await!(fs.device.write(&mut write_buf, offset, BLOCK_SIZE));
+            await!(fs.device.write(&mut write_buf, offset, blk_size));
             written += num_bytes;
         }
         let last_byte = offset + written;
@@ -258,10 +263,12 @@ impl Inode {
     }
 
     pub fn read(&self, offset: usize, data: &mut [u8]) -> usize {
+        let fs = fs_internal.unwrap();
+        let blk_size = fs.device.blk_size();
         let mut read = 0;
-        let mut block_offset = offset % BLOCK_SIZE; // offset from first block
-        let start = offset / BLOCK_SIZE; // first block to act on
-        let blocks_to_act_on = ceil_div(block_offset + data.len(), BLOCK_SIZE);
+        let mut block_offset = offset % blk_size; // offset from first block
+        let start = offset / blk_size; // first block to act on
+        let blocks_to_act_on = ceil_div(block_offset + data.len(), blk_size);
 
         for i in 0..blocks_to_act_on {
             // Resetting the block offset after first pass since we want to read from
@@ -274,14 +281,14 @@ impl Inode {
             let num_bytes = if i == blocks_to_act_on - 1 {
                 data.len() - read
             } else {
-                BLOCK_SIZE - block_offset
+                blk_size - block_offset
             };
 
             let page = self.get_page(start + i);
-            let pg_offset = self.fs.data_base + page * BLOCK_SIZE;
+            let pg_offset = self.fs.data_base + page * blk_size;
             let mut read_buf = spdk_rs::env::dma_zmalloc(self.fs.device.blk_size(), 0);
-            self.fs.device.read(&mut read_buf, pg_offset, BLOCK_SIZE);
-            let disk_page = read_buf.read_bytes(BLOCK_SIZE);
+            self.fs.device.read(&mut read_buf, pg_offset, blk_size);
+            let disk_page = read_buf.read_bytes(blk_size);
             // TODO: check compatability here
 
             let slice = &mut data[read..(read + num_bytes)];
